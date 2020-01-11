bf6e20b6e5a21552777ba36d5a2a45220b2e122d
software8686 <software8686@gmail.com>
2020-01-11 09:00:09 +0330

    Commit> Notification> LargeIcon Notification

--- app/src/main/java/ir/ea2/android_notification/CustomNotification.java
+++ app/src/main/java/ir/ea2/android_notification/CustomNotification.java
@@ -4,6 +4,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.TaskStackBuilder;
@@ -19,10 +20,10 @@ public class CustomNotification {
         return ourInstance;
     }
 
-
     public void sampleNotification(Context context){
         //Set NotificationId : This is For Avoid From Conflicted Notification's Together .
         int notificationId = 1001;
+        int requestPendingIntent=100;
         //Set Notification's Option's
         NotificationCompat.Builder notification = new NotificationCompat.Builder(context).setSmallIcon(R.mipmap.ic_launcher)
                 .setContentText(context.getResources().getString(R.string.notification_text))
@@ -31,11 +32,42 @@ public class CustomNotification {
                 .setColor(context.getResources().getColor(R.color.notification));
         //Set Intent For Using In TaskStackBuilder .
         Intent intent = new Intent(context, SecondaryActivity.class);
+        // Set backIntent For Going Previous Activity .
+        Intent backIntent = new Intent(context, MainActivity.class);
+        backIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         //Set TaskStackBuilder For Using In PendingIntent .
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
         stackBuilder.addNextIntent(intent);
         //Set PendingIntent For Using In setContentIntent() .
-        PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pendingIntent = PendingIntent.getActivities(context , requestPendingIntent++,new Intent[]{backIntent,intent},PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.setContentIntent(pendingIntent);
+        //Set NotificationManager For Running In Background .
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
+        //Call Notification .
+        notificationManager.notify(notificationId, notification.build());
+    }
+
+    public void largeIconNotification(Context context){
+        //Set NotificationId : This is For Avoid From Conflicted Notification's Together .
+        int notificationId = 1001;
+        int requestPendingIntent=100;
+        //Set Notification's Option's
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(context).setSmallIcon(R.mipmap.ic_launcher)
+                .setContentText(context.getResources().getString(R.string.notification_text))
+                .setContentTitle(context.getResources().getString(R.string.notification_title))
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(),R.drawable.notification))
+                .setColor(context.getResources().getColor(R.color.notification));
+        //Set Intent For Using In TaskStackBuilder .
+        Intent intent = new Intent(context, SecondaryActivity.class);
+        // Set backIntent For Going Previous Activity .
+        Intent backIntent = new Intent(context, MainActivity.class);
+        backIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        //Set TaskStackBuilder For Using In PendingIntent .
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addNextIntent(intent);
+        //Set PendingIntent For Using In setContentIntent() .
+        PendingIntent pendingIntent = PendingIntent.getActivities(context , requestPendingIntent++,new Intent[]{backIntent,intent},PendingIntent.FLAG_UPDATE_CURRENT);
         notification.setContentIntent(pendingIntent);
         //Set NotificationManager For Running In Background .
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
--- app/src/main/java/ir/ea2/android_notification/MainActivity.java
+++ app/src/main/java/ir/ea2/android_notification/MainActivity.java
@@ -4,30 +4,54 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     Context context = this;
-    private Button btnSampleNotification;
+    private Button btnShowNotification;
+    private RadioButton rdBtnSample, rdBtnLargeIcon;
+    private RadioGroup rdGroup;
+    private int checkedRadioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setViews();
-        clickListeners();
+        clickListener();
     }
 
-    private void clickListeners() {
-        btnSampleNotification.setOnClickListener(new View.OnClickListener() {
+    private void clickListener() {
+        btnShowNotification.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                CustomNotification.getInstance().sampleNotification(context);
+                checkedRadioButton = rdGroup.getCheckedRadioButtonId();
+                switch (checkedRadioButton) {
+                    case R.id.ac_main_rdBtn_sampleNotification: {
+                        CustomNotification.getInstance().sampleNotification(context);
+                    }
+                    break;
+                    case R.id.ac_main_rdBtn_largeIconNotification: {
+                        CustomNotification.getInstance().largeIconNotification(context);
+                    }
+                    break;
+                    default:
+                        Toast.makeText(context, "Please , Select One Item!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
 
     private void setViews() {
-        btnSampleNotification = (Button) findViewById(R.id.main_btn_sample_notification);
+        btnShowNotification = (Button) findViewById(R.id.main_btn_showNotification);
+        rdBtnLargeIcon = (RadioButton) findViewById(R.id.ac_main_rdBtn_largeIconNotification);
+        rdBtnSample = (RadioButton) findViewById(R.id.ac_main_rdBtn_sampleNotification);
+        rdGroup = (RadioGroup) findViewById(R.id.ac_main_rdGroup);
     }
+
+
 }
